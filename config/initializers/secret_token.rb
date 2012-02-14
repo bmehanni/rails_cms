9137138e50a3d18782fbadd013cdabcbf141c6c3
Marko Iskander <markiskander@gmail.com>
2012-02-13 22:09:17 -0500

    Intial rails shell, straight scafolding only.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCms::Application.config.secret_token = 'a178267661467668b3a70051d661da23f3d6d9db4ade4473728700960ecf1982e0c22b1e5102444d9c5fce0291e6cb1061450a5bd9b85ebb1a6fee01464022fd'
